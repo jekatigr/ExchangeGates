b326a0ffee030e2b1b5bf377b65fc73285257f3e
jekatigr <jekatigr@yandex.ru>
2018-09-27 20:48:44 +0300

    добавлены тесты

---
+++ tidex-api/__tests__/index.js
@@ -0,0 +1,78 @@
+const TidexApi = require('../index');
+
+const Market = require('../models/Market');
+const Ticker = require('../models/Ticker');
+const OrderBook = require('../models/OrderBook');
+
+const api = new TidexApi();
+
+describe('Tidex API', () => {
+    it('should create api instance', () => {
+        expect(api).toBeInstanceOf(TidexApi);
+    });
+
+    it('should load and return markets', async () => {
+        api.markets = undefined;
+        expect(api.markets).toBeUndefined();
+        const markets = await api.getMarkets();
+        expect(api.markets).toBeInstanceOf(Array);
+        expect(markets).toBeInstanceOf(Array);
+        expect(markets.length).toBeGreaterThan(0);
+        markets.forEach(m => expect(m).toBeInstanceOf(Market));
+    });
+
+    describe('fetch tickers', () => {
+        it('should load markets before fetch', async () => {
+            api.markets = undefined;
+            expect(api.markets).toBeUndefined();
+            const tickers = await api.getTickers();
+            expect(api.markets).toBeInstanceOf(Array);
+            api.markets.forEach(m => expect(m).toBeInstanceOf(Market));
+        });
+
+        it('should fetch all', async () => {
+            const tickers = await api.getTickers();
+            expect(tickers).toBeInstanceOf(Array);
+            expect(tickers.length).toBeGreaterThan(0);
+            tickers.forEach(t => expect(t).toBeInstanceOf(Ticker));
+        });
+
+        it('should fetch two', async () => {
+            const markets = await api.getMarkets();
+            const symbols = markets.slice(0, 2).map(m => `${m.base}/${m.quote}`);
+
+            const tickers = await api.getTickers(symbols);
+            expect(tickers).toBeInstanceOf(Array);
+            expect(tickers).toHaveLength(2);
+            tickers.forEach(t => expect(t).toBeInstanceOf(Ticker));
+        });
+    });
+
+    describe('fetch orderbooks', () => {
+        it('should load markets before fetch', async () => {
+            api.markets = undefined;
+            expect(api.markets).toBeUndefined();
+            const orderbooks = await api.getOrderBooks();
+            expect(api.markets).toBeInstanceOf(Array);
+            expect(api.markets.length).toBeGreaterThan(0);
+            api.markets.forEach(m => expect(m).toBeInstanceOf(Market));
+        });
+
+        it('should fetch all', async () => {
+            const orderbooks = await api.getOrderBooks();
+            expect(orderbooks).toBeInstanceOf(Array);
+            expect(orderbooks.length).toBeGreaterThan(0);
+            orderbooks.forEach(o => expect(o).toBeInstanceOf(OrderBook));
+        });
+
+        it('should fetch three', async () => {
+            const markets = await api.getMarkets();
+            const symbols = markets.slice(0, 3).map(m => `${m.base}/${m.quote}`);
+
+            const orderbooks = await api.getOrderBooks(symbols);
+            expect(orderbooks).toBeInstanceOf(Array);
+            expect(orderbooks).toHaveLength(3);
+            orderbooks.forEach(o => expect(o).toBeInstanceOf(OrderBook));
+        });
+    });
+});
