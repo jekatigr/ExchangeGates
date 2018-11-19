6a56bafa3ac5e1c09e06a06fd1a3b60852ae4500
jekatigr <jekatigr@yandex.ru>
2018-11-19 19:07:39 +0300

    получение ордеров для bibox

--- src/exchangeServices/BiboxApiService.js
+++ src/exchangeServices/BiboxApiService.js
@@ -269,4 +269,60 @@ module.exports = class BiboxApiService extends ExchangeServiceAbstract {
             throw new Error(`Exception while fetching balances, ex: ${ex}`);
         }
     }
+
+    async getActiveOrders(symbol) {
+        try {
+            this.rotateAgent();
+            const openOrders = await this.api.fetchOpenOrders(symbol) || [];
+            return openOrders.map(o => ({
+                id: o.id,
+                base: o.symbol.split('/')[0],
+                quote: o.symbol.split('/')[1],
+                operation: o.side,
+                amount: o.amount,
+                remain: (o.remaining === undefined) ? o.amount : o.remaining,
+                price: o.price,
+                average: o.average,
+                created: o.timestamp,
+                status: 'active'
+            }));
+        } catch (ex) {
+            console.log(`Exception while fetching active orders, ex: ${ex}, stacktrace: ${ex.stack}`);
+            throw new Error(`Exception while fetching active orders, ex: ${ex}`);
+        }
+    }
+
+    async getOrders(ids = []) {
+        if (ids.length === 0) {
+            console.log('Exception while getting orders, params missing');
+            throw new Error('Exception while getting orders, params missing');
+        }
+
+        const result = [];
+        /* eslint-disable no-await-in-loop */
+        for (const orderId of ids) {
+            try {
+                this.rotateAgent();
+                const o = await this.api.fetchOrder(orderId);
+                result.push({
+                    success: true,
+                    id: o.id,
+                    base: o.symbol.split('/')[0],
+                    quote: o.symbol.split('/')[1],
+                    operation: o.side,
+                    amount: o.amount,
+                    remain: (o.remaining === undefined) ? o.amount : o.remaining,
+                    price: o.price,
+                    average: o.average,
+                    created: o.timestamp,
+                    status: (o.status === 'open') ? 'active' : o.status
+                });
+            } catch (ex) {
+                console.log(`Exception while getting order, ex: ${ex}, stacktrace: ${ex.stack}`);
+                result.push({ id: orderId, success: false, error: ex.message });
+            }
+        }
+        /* eslint-enable no-await-in-loop */
+        return result;
+    }
 };
